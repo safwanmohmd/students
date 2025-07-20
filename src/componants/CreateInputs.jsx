41f91367b43a919d0092057b471681e11e6db264
safwan <safwankilinakode53@gmail.com>
2025-07-20 22:59:14 +0530

    form validation

--- src/componants/CreateInputs.jsx
+++ src/componants/CreateInputs.jsx
@@ -1,21 +1,21 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 
-import { createStudent } from '../features/studentSlice';
+import { createStudent } from "../features/studentSlice";
 
 const CreateInputs = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [phone, setPhone] = useState('');
-  const [website, setWebsite] = useState('');
-  const [username, setUsername] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [website, setWebsite] = useState("");
+  const [username, setUsername] = useState("");
   const dispatch = useDispatch();
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     if (!name || !email || !phone || !website || !username) {
-     alert("Please fill out all fields");
+      alert("Please fill out all fields");
       return;
     }
 
@@ -29,13 +29,13 @@ const CreateInputs = () => {
     };
 
     dispatch(createStudent(newStudent));
-   alert("student created Successfully");
+    alert("student created Successfully");
 
-    setName('');
-    setEmail('');
-    setPhone('');
-    setWebsite('');
-    setUsername('');
+    setName("");
+    setEmail("");
+    setPhone("");
+    setWebsite("");
+    setUsername("");
   };
 
   return (
@@ -45,6 +45,8 @@ const CreateInputs = () => {
         className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mt-6 px-4"
       >
         <input
+          required
+          minLength={5}
           type="text"
           value={name}
           onChange={(e) => setName(e.target.value)}
@@ -52,32 +54,40 @@ const CreateInputs = () => {
           className="px-4 py-2 border border-green-300 rounded-lg"
         />
         <input
-          type="text"
+          required
+          type="email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           placeholder="email"
           className="px-4 py-2 border border-green-300 rounded-lg"
         />
         <input
-          type="number"
+          type="tel"
           value={phone}
           onChange={(e) => setPhone(e.target.value)}
-          placeholder="Phone "
+          placeholder="Phone"
           className="px-4 py-2 border border-green-300 rounded-lg"
+          required
+          pattern="\d{10}"
+          title="Phone number must be exactly 10 digits"
         />
         <input
           type="text"
+          minLength={5}
           value={website}
           onChange={(e) => setWebsite(e.target.value)}
           placeholder="Website"
           className="px-4 py-2 border border-green-300 rounded-lg"
+          required
         />
         <input
           type="text"
+          minLength={5}
           value={username}
           onChange={(e) => setUsername(e.target.value)}
           placeholder="Username"
           className="px-4 py-2 border border-green-300 rounded-lg lg:col-span-2"
+          required
         />
         <button
           type="submit"
@@ -86,7 +96,6 @@ const CreateInputs = () => {
           Create student
         </button>
       </form>
-    
     </>
   );
 };
